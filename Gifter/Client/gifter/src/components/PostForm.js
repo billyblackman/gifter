1c0f56831d4711d940456561b3f115fc91be820a
Billy Blackman <blackmanbilly@gmail.com>
2020-07-02 15:09:36 -0500

    Created react app

---
+++ Gifter/Client/gifter/src/components/PostForm.js
@@ -0,0 +1,71 @@
+import React, { useContext, useRef } from "react"
+import { Form, FormGroup, Label, Input, Button } from "reactstrap"
+import { PostContext } from "../providers/PostProvider"
+
+export const NewPostForm = () => {
+
+
+    const { addPost } = useContext(PostContext)
+
+    const title = useRef()
+    const imageUrl = useRef()
+    const caption = useRef()
+
+    const newDate = new Date()
+    const dateCreated = newDate.toJSON()
+    
+    const userProfile = 1
+
+    const addPostItem = () => {
+
+        const newPost = {
+            title: title.current.value,
+            imageUrl: imageUrl.current.value,
+            caption: caption.current.value,
+            dateCreated: dateCreated,
+            userProfile: userProfile
+        }
+        console.log(newPost)
+        addPost(newPost)
+        debugger
+    }
+
+    return (
+        <Form>
+            <FormGroup>
+                <Label htmlFor="title">Title</Label>
+                <Input type="text"
+                        name="title"
+                        innerRef={title}
+                        required
+                        autoFocus
+                        placeholder="title"></Input>
+            </FormGroup>
+            <FormGroup>
+                <Label htmlFor="imageUrl">Image URL</Label>
+                <Input type="text"
+                        name="imageUrl"
+                        innerRef={imageUrl}
+                        required
+                        autoFocus
+                        placeholder="imageUrl"></Input>
+            </FormGroup>
+            <FormGroup>
+                <Label htmlFor="caption">Caption</Label>
+                <Input type="textarea"
+                        name="caption"
+                        innerRef={caption}
+                        required
+                        autoFocus
+                        placeholder="caption"></Input>
+            </FormGroup>
+            <Button color="success" type="submit"
+                onClick={event => {
+                    event.preventDefault()
+                    addPostItem()
+                }}>
+                Save Post
+            </Button>
+        </Form>
+    )
+}
